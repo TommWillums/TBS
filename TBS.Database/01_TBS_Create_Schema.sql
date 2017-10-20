8754ac55533ca30bb018d60c10733cda06f474fb
TommWillums <tomm@willums.no>
2017-10-20 14:40:19 +0200

    Added TBS.Database and initial SQL-scripts

---
+++ TBS.Database/01_TBS_Create_Schema.sql
@@ -0,0 +1,31 @@
+﻿--=============================================================================
+-- 01_TBS_Create_Schema.sql
+--=============================================================================
+use TBS
+go
+
+if (exists(select * from sys.tables where name = 'Clubs'))
+	drop table Clubs;
+
+if (exists(select * from sys.tables where name = 'Users'))
+	drop table Users;
+go
+
+create table Clubs (
+	Id				int			 not null identity primary key,
+	Name			varchar(50)  not null,
+	Contact			varchar(50)	 not null,
+	Address			varchar(100) not null,
+	CourtCount		int			 not null,
+	Subscription	varchar(20)	 null,
+	AutoRenewal		bit			 not null default 1,
+	NextRenewalDate	DateTime2	 null,
+	Price			money		 not null default 0.0,
+	Created			DateTime2	 not null default GetDate()
+)
+
+create table Users (
+	Id			int			not null identity primary key,
+	Name		varchar(50)	not null
+)
+
