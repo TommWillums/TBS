7241b43f33a8bcd5f66d8dee1e7090cedaacf4af
Tomm <tomm.schuller@outlook.com>
2017-10-21 17:37:22 +0200

    Added dapper call to database and TestMethod to access it

--- TBS.Test/UnitTest1.cs
+++ TBS.Test/UnitTest1.cs
@@ -1,5 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
 using TBS.Domain;
+using TBS.Persistence;
 
 namespace TBS.Test
 {
@@ -11,6 +13,13 @@ namespace TBS.Test
         {
             var calc = new Club();
             Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        public async Task Get_Club_100_From_Db()
+        {
+            Club club = await new ClubsDb().Get(100);
+            Assert.AreEqual(club.Id, 100);
         }
     }
 }
---
+++ TBS/Persistence/ClubsDb.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using TBS.Domain;
+using System.Data.SqlClient;
+
+namespace TBS.Persistence
+{
+    public class ClubsDb
+    {
+        public async Task<Club> Get(int id)
+        {
+            Club club = null;
+
+            using (SqlConnection conn = new SqlConnection("Server=.;Database=TBS;Integrated Security=True"))
+            {
+                try { 
+                    conn.Open();
+                    club = await conn.QuerySingleAsync<Club>("select * from Clubs where Id = @Id", new { Id = id });
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+
+            return club;
+        }
+
+    }
+}
