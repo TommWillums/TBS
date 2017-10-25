98209040e89bd60cfdde404c5e326655c32ae9b9
TommWillums <tomm@willums.no>
2017-10-25 10:28:38 +0200

    SaveClub

--- TBS/Data/Commands/Clubs/SaveClub.cs
+++ TBS/Data/Commands/Clubs/SaveClub.cs
@@ -7,22 +7,23 @@ namespace TBS.Data.Commands.Clubs
 {
     public class SaveClub : ICommand
     {
-        private readonly Club _animal;
+        private readonly Club _club;
 
         public SaveClub(Club animal)
         {
-            _animal = animal;
+            _club = animal;
         }
 
         public void Execute(ISession session)
         {
-            if (_animal.Id > 0)
+            if (_club.Id > 0)
             {
-                session.Execute("UPDATE Clubs SET Name = @Name, CommonName = @CommonName WHERE Id = @Id", new { _animal.Id, _animal.Name, _animal.CommonName });
+                session.Execute("UPDATE Clubs SET Name = @Name, CommonName = @CommonName WHERE Id = @Id", new { _club.Id, _club.Name, _club.CommonName });
                 return;
             }
 
-            session.Execute("INSERT INTO Clubs (Name, CommonName) VALUES (@Name, @CommonName)", new { _animal.Name, _animal.CommonName });
+            session.Execute("insert into Clubs (ClubName, ShortName, Contact) values (@ClubName, @ShortName, @Contact)",
+                new { _club.ClubName, _club.ShortName, _club.Contact });
         }
     }
 
